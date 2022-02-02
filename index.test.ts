114416e089d41315fd292e509b8188b6f41b4b23
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-02-02 10:43:46 +0000

    refactor: update for compliance with eslint and prettier config

--- index.test.ts
+++ index.test.ts
@@ -2,11 +2,5 @@ import test from "ava";
 import chain from "./index";
 
 test("value", t => t.is(chain("giraffe").value, "giraffe"));
-test("map", t => t.is(chain("guinness")
-    .map(s => s.replace(/u|ne|ss/g, ""))
-    .value, "gin"));
-test("then", t =>
-    t.is(chain(123)
-        .then(n => chain(456)
-            .map(m => n + m))
-        .value, 579));
+test("map", t => t.is(chain("guinness").map(s => s.replace(/u|ne|ss/gu, "")).value, "gin"));
+test("then", t => t.is(chain(123).then(n => chain(456).map(m => n + m)).value, 579));
