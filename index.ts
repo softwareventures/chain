93229eee74be2f7169ea897eefeb396695b0e709
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-12-11 20:25:44 +0000

    feat: remove default export in favour of named export
    BREAKING CHANGE: There is no longer a default export. Use the named export instead, e.g. use `import
    {chain} from "@softwareventures/chain"` instead of `import chain from "@softwareventures/chain"`.

--- index.ts
+++ index.ts
@@ -1,22 +1,13 @@
-interface Chain<T> {
+export interface Chain<T> {
     readonly value: T;
     map: <U>(f: (x: T) => U) => Chain<U>;
     then: <U>(f: (x: T) => Chain<U>) => Chain<U>;
 }
 
-function chain<T>(x: T): Chain<T> {
+export function chain<T>(x: T): Chain<T> {
     return {
         value: x,
         map: f => chain(f(x)),
         then: f => f(x)
     };
 }
-
-// For forward compatibility with v3.0
-chain.chain = chain;
-
-/** @deprecated For backwards compatibility with versions < v1.0.1.
- * Will be removed in v3.0. */
-chain.default = chain;
-
-export = chain;
